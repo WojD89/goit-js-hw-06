445294409141613e16ed778e319f8aa623930f77
WojD89 <wojciech.dyrka@gmail.com>
2023-12-03 19:42:57 +0100

    tasks 1-9 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailInput = this.elements.email;
+  const passwordInput = this.elements.password;
+
+  if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") {
+    alert("All fields should be completed!");
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    console.log(formData);
+
+    this.reset();
+  }
+});
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColorBtn = document.querySelector(".change-color");
+const bgdColorSpan = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", function () {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  bgdColorSpan.textContent = randomColor;
+});
