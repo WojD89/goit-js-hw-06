5aa91d45abc7bfd7d44ef555d64c19209616bcb4
WojD89 <wojciech.dyrka@gmail.com>
2023-12-03 19:02:14 +0100

    update task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,11 @@
-const numOfLis = document.querySelector("li.item").length;
-console.log(numOfLis);
+const catList = document.querySelectorAll("li.item");
+const numOfCategories = catList.length;
+console.log(`Number of categories: ${numOfCategories}`);
+
+catList.forEach((category) => {
+  const header = category.querySelector("h2");
+  const categoryItems = category.querySelectorAll("li");
+
+  console.log(`Category: ${header.textContent}`);
+  console.log(`Elements: ${categoryItems.length}`);
+});
